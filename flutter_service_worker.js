28ea46ab4978d00a795c926ecc4b8578ae83b324
ِِAhmed Mohmmed Salah <145204469+AhmedSalah-26@users.noreply.github.com>
2024-09-08 02:50:31 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "01f0836eee015cbc2526f9183bd6cfa8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "061d9782452eb027b2313922bf9584e2",
+"flutter_bootstrap.js": "e2e3131e4572ab7cff30d8dc5cb3e535",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
